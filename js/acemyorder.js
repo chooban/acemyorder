a16e19f9884d72ce123a0964c9fa5023221509ac
Ross Hendry <ross@rosshendry.com>
2012-03-10 10:18:11 +0000

    Reorganisation of js

--- js/acemyorder.js
+++ js/acemyorder.js
@@ -3,217 +3,55 @@ var rePrice = /(\d+(\.\d\d)?)/;
 
 var orderData = [];
 
-// Nabbed from stack overflow
-function findByPreviewsID(arr, closure, previewsId ) {
+function findByPreviewsID(arr, previewsId ) {
 	// Set up return value.
 	var retVal = -1;
 	$.each(arr,function(idx, val){
-		// option 2.  Run the closure:
-		if ( closure( val, previewsId ) ) {
+		if ( val.previews == previewsId ) {
 			retVal = idx;
 		}
 	});
 	return retVal;
 }
 
-var closure = function( item, previewsId ) {
-	return item.previews == previewsId;
-}
-
-/**
-*
-*  Base64 encode / decode
-*  http://www.webtoolkit.info/
-*
-**/
-
-var Base64 = {
-
-	// private property
-	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-	// public method for encoding
-	encode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-		var i = 0;
-
-		input = Base64._utf8_encode(input);
-
-		while (i < input.length) {
-
-			chr1 = input.charCodeAt(i++);
-			chr2 = input.charCodeAt(i++);
-			chr3 = input.charCodeAt(i++);
-
-			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
-
-			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
-			} else if (isNaN(chr3)) {
-				enc4 = 64;
-			}
-
-			output = output +
-			this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-			this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-
-		}
-
-		return output;
-	},
-
-	// public method for decoding
-	decode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0;
-
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-		while (i < input.length) {
-
-			enc1 = this._keyStr.indexOf(input.charAt(i++));
-			enc2 = this._keyStr.indexOf(input.charAt(i++));
-			enc3 = this._keyStr.indexOf(input.charAt(i++));
-			enc4 = this._keyStr.indexOf(input.charAt(i++));
-
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
-
-			output = output + String.fromCharCode(chr1);
-
-			if (enc3 != 64) {
-				output = output + String.fromCharCode(chr2);
-			}
-			if (enc4 != 64) {
-				output = output + String.fromCharCode(chr3);
-			}
-
-		}
-
-		output = Base64._utf8_decode(output);
-
-		return output;
-
-	},
-
-	// private method for UTF-8 encoding
-	_utf8_encode : function (string) {
-		string = string.replace(/\r\n/g,"\n");
-		var utftext = "";
-
-		for (var n = 0; n < string.length; n++) {
-
-			var c = string.charCodeAt(n);
-
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-
-		}
-
-		return utftext;
-	},
-
-	// private method for UTF-8 decoding
-	_utf8_decode : function (utftext) {
-		var string = "";
-		var i = 0;
-		var c = c1 = c2 = 0;
-
-		while ( i < utftext.length ) {
-
-			c = utftext.charCodeAt(i);
-
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			}
-			else if((c > 191) && (c < 224)) {
-				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-				i += 2;
-			}
-			else {
-				c2 = utftext.charCodeAt(i+1);
-				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
-
-		}
-
-		return string;
-	}
-
-}
 
 jQuery.extend({
- csv: function(delim, quote, linedelim) {
-     delim = typeof delim == "string" ? new RegExp( "[" + (delim || ","   ) + "]" ) : typeof delim == "undefined" ? ","    : delim;
-     quote = typeof quote == "string" ? new RegExp("^[" + (quote || '"'   ) + "]" ) : typeof quote == "undefined" ? '"'    : quote;
-     lined = typeof lined == "string" ? new RegExp( "[" + (lined || "\r\n") + "]+") : typeof lined == "undefined" ? "\r\n" : lined;
+	csv : function(delim, quote, linedelim) {
+		delim = typeof delim == "string" ? new RegExp("[" + (delim || ","   ) + "]") : typeof delim == "undefined" ? "," : delim;
+		quote = typeof quote == "string" ? new RegExp("^[" + (quote || '"'   ) + "]") : typeof quote == "undefined" ? '"' : quote;
+		lined = typeof lined == "string" ? new RegExp("[" + (lined || "\r\n") + "]+") : typeof lined == "undefined" ? "\r\n" : lined;
 
-     function splitline (v) {
-         // Split the line using the delimitor
-         var arr  = v.split(delim), out = [], q;
-         for (var i=0, l=arr.length; i<l; i++) {
-             if (q = arr[i].match(quote)) {
-                 for (j=i; j<l; j++) {
-                     if (arr[j].charAt(arr[j].length-1) == q[0]) { break; }
-                 }
-                 var s = arr.slice(i,j+1).join(delim);
-                 out.push(s.substr(1,s.length-2));
-                 i = j;
-             }
-             else { out.push(arr[i]); }
-         }
+		function splitline(v) {
+			// Split the line using the delimitor
+			var arr = v.split(delim), out = [], q;
+			for(var i = 0, l = arr.length; i < l; i++) {
+				if( q = arr[i].match(quote)) {
+					for( j = i; j < l; j++) {
+						if(arr[j].charAt(arr[j].length - 1) == q[0]) {
+							break;
+						}
+					}
+					var s = arr.slice(i, j + 1).join(delim);
+					out.push(s.substr(1, s.length - 2));
+					i = j;
+				} else {
+					out.push(arr[i]);
+				}
+			}
 
-         return out;
-     }
+			return out;
+		}
 
-     return function(text) {
-         var lines = text.split(lined);
-         for (var i=0, l=lines.length; i<l; i++) {
-             lines[i] = splitline(lines[i]);
-         }
-         return lines;
-     };
- }
+		return function(text) {
+			var lines = text.split(lined);
+			for(var i = 0, l = lines.length; i < l; i++) {
+				lines[i] = splitline(lines[i]);
+			}
+			return lines;
+		};
+	}
 });
 
-function stringToBytes ( str ) {
-  var ch, st, re = [];
-  for (var i = 0; i < str.length; i++ ) {
-    ch = str.charCodeAt(i);  // get char
-    st = [];                 // set up "stack"
-    do {
-      st.push( ch & 0xFF );  // push byte to stack
-      ch = ch >> 8;          // shift value down by 1 byte
-    }
-    while ( ch );
-    // add stack contents to result
-    // done because chars have "wrong" endianness
-    re = re.concat( st.reverse() );
-  }
-  // return an array of bytes
-  return re;
-}
 
 function csv2datatable( sURL ) {
 	$.ajax(
@@ -355,7 +193,7 @@ function deleteFromOrder( iRow ) {
 		"bRetrieve": true,
 	} );
 	var aData = table.fnGetData( iRow );
-	var idx = findByPreviewsID( orderData, closure, aData[0] );
+	var idx = findByPreviewsID( orderData, aData[0] );
 
 	if ( idx > -1 ) {
 		orderData.splice( idx, 1 );
@@ -381,21 +219,6 @@ function calculateTotals() {
 	$('#numitems').html( numItems );
 	$('#numtitles').html( numTitles );
 	$('#ordertotal').html( (totalCost/100).toFixed( 2 ) );
-}
-
-function insertionSort( arr ) {
-	for(var j = 1; j < arr.length; j++) {
-		var key = arr[j];
-		var i = j - 1;
-
-		while(i >= 0 && arr[i] > key) {
-			arr[i+1] = arr[i];
-			i = i - 1;
-		}
-
-		arr[i+1] = key;
-	}
-	return arr;
 }
 
 function calculateOrder() {
@@ -462,7 +285,7 @@ function calculateOrder() {
 			event.stopPropagation();
 			var previewsId = /id_(.*)/.exec( event.target.id )[1];
 			
-			var idx = findByPreviewsID( orderData, closure, previewsId );
+			var idx = findByPreviewsID( orderData, previewsId );
 
 			if ( idx > -1 ) {
 				orderData.splice( idx, 1 );
