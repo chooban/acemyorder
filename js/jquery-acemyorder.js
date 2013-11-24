3f6dac5b84c7397e0bdafa5330aae9f3e278f286
Ross Hendry <ross@rosshendry.com>
2013-11-24 15:42:10 +0000

    Big refactor
    Stop doing as much text processing in Javascript, instead
    it's now done on the server side.

---
+++ js/jquery-acemyorder.js
@@ -0,0 +1,39 @@
+/**
+ * Since this is an extension for jQuery it belongs in its own file.
+ */
+jQuery.extend({
+	csv : function(delim, quote, linedelim) {
+		delim = typeof delim == "string" ? new RegExp("[" + (delim || ","   ) + "]") : typeof delim == "undefined" ? "," : delim;
+		quote = typeof quote == "string" ? new RegExp("^[" + (quote || '"'   ) + "]") : typeof quote == "undefined" ? '"' : quote;
+		lined = typeof lined == "string" ? new RegExp("[" + (lined || "\r\n") + "]+") : typeof lined == "undefined" ? "\r\n" : lined;
+
+		function splitline(v) {
+			// Split the line using the delimitor
+			var arr = v.split(delim), out = [], q;
+			for(var i = 0, l = arr.length; i < l; i++) {
+				if( q = arr[i].match(quote)) {
+					for( j = i; j < l; j++) {
+						if(arr[j].charAt(arr[j].length - 1) == q[0]) {
+							break;
+						}
+					}
+					var s = arr.slice(i, j + 1).join(delim);
+					out.push(s.substr(1, s.length - 2));
+					i = j;
+				} else {
+					out.push(arr[i]);
+				}
+			}
+
+			return out;
+		}
+
+		return function(text) {
+			var lines = text.split(lined);
+			for(var i = 0, l = lines.length; i < l; i++) {
+				lines[i] = splitline(lines[i]);
+			}
+			return lines;
+		};
+	}
+});
