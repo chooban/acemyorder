90009ed61653703f7673e5c67d65df7c1e7caade
Ross Hendry <ross@rosshendry.com>
2013-12-21 18:36:43 +0000

    Formatted

--- js/acemyorder.js
+++ js/acemyorder.js
@@ -1,7 +1,7 @@
 // There's got to be a better way than a global variable.
 var order = new CustomerOrder();
 
-function csv2datatable( sURL ) {
+function csv2datatable(sURL) {
 
   /**
    * I *will* fix this!
@@ -14,145 +14,133 @@ function csv2datatable( sURL ) {
   previewsToMonth["469"] = "APR14";
   previewsToMonth["470"] = "MAY14";
 
-	$.ajax(
-		{
-			url : "csvfilter.php",
-			scriptCharset : "UTF-8",
-			type : "GET",
-			dataType : "json",
-			success : function( data, textStatus, jqXHR ) {
+  $.ajax({
+    url: "csvfilter.php",
+    scriptCharset: "UTF-8",
+    type: "GET",
+    dataType: "json",
+    success: function(data, textStatus, jqXHR) {
 
-        var previewsIssue = data['file'];
-        order.issue = previewsIssue;
+      var previewsIssue = data['file'];
+      order.issue = previewsIssue;
 
-	 			// Set the "Now displaying..." text
-	 			$('#nowdisplaying').text( "Displaying " + previewsIssue );
-	 			$('#directlink').html( "<a href=\"csv/" + previewsIssue + ".csv\">Direct link to csv</a>" );
-				var csvdata = jQuery.csv()( data['contents'] );
+      // Set the "Now displaying..." text
+      $('#nowdisplaying').text("Displaying " + previewsIssue);
+      $('#directlink').html("<a href=\"csv/" + previewsIssue + ".csv\">Direct link to csv</a>");
+      var csvdata = jQuery.csv()(data['contents']);
 
-				// The first row's not useful as it's the header row
-				csvdata.pop();
+      // The first row's not useful as it's the header row
+      csvdata.pop();
 
-				var row;
-				var was;
-				var match;
+      var row;
+      var was;
+      var match;
 
-        var month = previewsToMonth[ previewsIssue.slice( -3 ) ];
+      var month = previewsToMonth[previewsIssue.slice(-3)];
 
-				var buttonColTemplate = $.createTemplate( '<input type="checkbox" id="row{$T.rowId}" value="previews_{$T.previewsId}" class="addtoorder"/>' );
-        var previewsLinkTemplate = $.createTemplate( '<a href="http://www.previewsworld.com/Home/1/1/71/916?stockItemID={$T.itemID}">{$T.displayText}</a>' );
+      var buttonColTemplate = $.createTemplate('<input type="checkbox" id="row{$T.rowId}" value="previews_{$T.previewsId}" class="addtoorder"/>');
+      var previewsLinkTemplate = $.createTemplate('<a href="http://www.previewsworld.com/Home/1/1/71/916?stockItemID={$T.itemID}">{$T.displayText}</a>');
 
-				for ( var i = 0; i < csvdata.length; i++ ) {
-					row = new Array();
+      for (var i = 0; i < csvdata.length; i++) {
+        row = new Array();
 
-					// Previews code
-					// Title
-					// Price
-					row[0] = csvdata[i][0];
-					row[1] = csvdata[i][1];
-					row[2] = csvdata[i][3] != null ? parseFloat( csvdata[i][3] ) : "";
+        // Previews code
+        // Title
+        // Price
+        row[0] = csvdata[i][0];
+        row[1] = csvdata[i][1];
+        row[2] = csvdata[i][3] != null ? parseFloat(csvdata[i][3]) : "";
 
-					// Reduced from
-					was = csvdata[i][5];
-					if ( was.length > 0 ) {
-						match = /(\d+(\.\d\d)?)/.exec( was );
-						row[3] = match[1];
-					}
-					else {
-						row[3] = "";
-					}
+        // Reduced from
+        was = csvdata[i][5];
+        if (was.length > 0) {
+          match = /(\d+(\.\d\d)?)/.exec(was);
+          row[3] = match[1];
+        }
+        else {
+          row[3] = "";
+        }
 
-					// Publisher
-					row[4] = csvdata[i][6].toLowerCase();
+        // Publisher
+        row[4] = csvdata[i][6].toLowerCase();
 
-					// What will eventually be the button
-					row[5] = "";
+        // What will eventually be the button
+        row[5] = "";
 
-					// Now overwrite the row in the array
-					csvdata[i] = row;
-				}
+        // Now overwrite the row in the array
+        csvdata[i] = row;
+      }
 
-				$("table#datatable").dataTable( {
-						"aaData": csvdata,
-						"iDisplayLength" : 30,
-						"bAutoWidth" : false,
-						"bDestroy" : true,
-						"aoColumnDefs" : [
-							{
-								"aTargets" : [ "description" ],
-								"sWidth" : "50%",
-							},
-							{
-								"aTargets": [ "publisher" ],
-								"sWidth" : "20%",
-								"sClass" : "publisher",
-							},
-							{
-								"aTargets": [ "price", "reduced", "previews", "buttoncol" ],
-								"sWidth" : "5%",
-							},
-							{
-								"aTargets" : [ "price", "reduced", "buttoncol" ],
-								"bSortable" : false,
-							},
-							{
-								"aTargets" : [ "price" ],
-								"fnRender" : function( oObj ) {
-									return "&pound;" + oObj.aData[2];
-								},
-							},
-							{
-								"aTargets" : [ "reduced" ],
-								"fnRender" : function( oObj ) {
-									return ( oObj.aData[3] != "" ? "&pound;" + oObj.aData[3] : "" );
-								},
-							},
-							{
-								"aTargets" : [ "buttoncol" ],
-								"fnRender" : function( oObj ) {
-									return $.processTemplateToText( 
-										buttonColTemplate, 
-										{ rowId : oObj.iDataRow,
-										  previewsId : oObj.aData[0] } 
-									);
-								},
-								"sClass" : "buttoncol",
-							},
-              {
-                "aTargets": [ "previews" ],
-                "mRender" : function( data, type, full ) {
-                  var id = full[0].slice( -4 );
-                  return $.processTemplateToText( 
-                    previewsLinkTemplate,
-                    {
-                      displayText: full[0],
-                      itemID: month + id,
-                    }
-                  );
-                },
-              },
-						],
+      $("table#datatable").dataTable({
+        "aaData": csvdata,
+        "iDisplayLength": 30,
+        "bAutoWidth": false,
+        "bDestroy": true,
+        "aoColumnDefs": [{
+            "aTargets": ["description"],
+            "sWidth": "50%",
+          }, {
+            "aTargets": ["publisher"],
+            "sWidth": "20%",
+            "sClass": "publisher",
+          }, {
+            "aTargets": ["price", "reduced", "previews", "buttoncol"],
+            "sWidth": "5%",
+          }, {
+            "aTargets": ["price", "reduced", "buttoncol"],
+            "bSortable": false,
+          }, {
+            "aTargets": ["price"],
+            "fnRender": function(oObj) {
+              return "&pound;" + oObj.aData[2];
+            },
+          }, {
+            "aTargets": ["reduced"],
+            "fnRender": function(oObj) {
+              return (oObj.aData[3] != "" ? "&pound;" + oObj.aData[3] : "");
+            },
+          }, {
+            "aTargets": ["buttoncol"],
+            "fnRender": function(oObj) {
+              return $.processTemplateToText(
+                buttonColTemplate, {
+                rowId: oObj.iDataRow,
+                previewsId: oObj.aData[0]
+              });
+            },
+            "sClass": "buttoncol",
+          }, {
+            "aTargets": ["previews"],
+            "mRender": function(data, type, full) {
+              var id = full[0].slice(-4);
+              return $.processTemplateToText(
+                previewsLinkTemplate, {
+                displayText: full[0],
+                itemID: month + id,
+              });
+            },
+          },
+        ],
 
-					}
-				);
-			},
-			error : function( jqXHR, textStatus, errorThrown ) {
-				alert( errorThrown );
-			}
-		}
-	);
+      });
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert(errorThrown);
+    }
+  });
 }
 
 /**
  * This isn't anything to do with calculate much of anything 
  * any more and should be renamed.
  */
+
 function calculateTotals() {
-  var formattedTotal = (order.getTotal()/100).toFixed( 2 );
-	$('#runningtotal').html( "&pound;" + formattedTotal );
-	$('#numitems').html( order.getNumItems() );
-	$('#numtitles').html( order.getNumTitles() );
-	$('#ordertotal').html( formattedTotal );
+  var formattedTotal = (order.getTotal() / 100).toFixed(2);
+  $('#runningtotal').html("&pound;" + formattedTotal);
+  $('#numitems').html(order.getNumItems());
+  $('#numtitles').html(order.getNumTitles());
+  $('#ordertotal').html(formattedTotal);
 }
 
 /**
@@ -160,150 +148,149 @@ function calculateTotals() {
  * of the selected items. From there, users can either get a CSV version
  * for emailing to Ace, or go back to the data table.
  */
+
 function calculateOrder() {
 
-	$('#dialogcontents').setTemplateURL( 'templates/ordertable.html' );
-	$('#dialogcontents').processTemplate( order.lineItems );
-	$('#dialogcontents').dialog( "open" );
+  $('#dialogcontents').setTemplateURL('templates/ordertable.html');
+  $('#dialogcontents').processTemplate(order.lineItems);
+  $('#dialogcontents').dialog("open");
 
-	// Now turn the quantity fields into spinners
-	$('.spinner').spinner( {
-		"min" : 1,
-    spin:function( event, ui ) {
+  // Now turn the quantity fields into spinners
+  $('.spinner').spinner({
+    "min": 1,
+    spin: function(event, ui) {
       // The ID of the spinner holds the Previews ID
-      var previewsId = /spinner_(.*)/.exec( this.id )[1];
-      order.setQuantity( previewsId, ui.value );
+      var previewsId = /spinner_(.*)/.exec(this.id)[1];
+      order.setQuantity(previewsId, ui.value);
       calculateTotals();
     },
-    change:function( event, ui ) {
-      $('tr.orderrow').each( function( i, tr ) {
-        var previewsId = /row_(.*)/.exec( tr.id )[1];
-        var inputs = tr.getElementsByTagName( "input" );
-        order.setQuantity( previewsId, $( inputs[0] ).spinner( "value" ) );
+    change: function(event, ui) {
+      $('tr.orderrow').each(function(i, tr) {
+        var previewsId = /row_(.*)/.exec(tr.id)[1];
+        var inputs = tr.getElementsByTagName("input");
+        order.setQuantity(previewsId, $(inputs[0]).spinner("value"));
       });
       calculateTotals();
     },
-	});
+  });
 
-	// And now that the button exists...
-	$('#submitorder').click( function( event ) {
+  // And now that the button exists...
+  $('#submitorder').click(function(event) {
 
-		// Iterate through the order table, set the quantities in orderData
-		$('tr.orderrow').each( function( i, tr ) {
-			var previewsId = /row_(.*)/.exec( tr.id )[1];
-			var inputs = tr.getElementsByTagName( "input" );
-			order.setQuantity( previewsId, $( inputs[0] ).spinner( "value" ) );
-      order.setComment( previewsId, '"' + $( inputs[1] ).val() + '"' );
-		});
+    // Iterate through the order table, set the quantities in orderData
+    $('tr.orderrow').each(function(i, tr) {
+      var previewsId = /row_(.*)/.exec(tr.id)[1];
+      var inputs = tr.getElementsByTagName("input");
+      order.setQuantity(previewsId, $(inputs[0]).spinner("value"));
+      order.setComment(previewsId, '"' + $(inputs[1]).val() + '"');
+    });
 
-		// Add the callback
-		$('#exportorder').submit(function() {
-			$( '#dialogcontents' ).dialog( "close" );
-			$( '#postsubmitmessage' ).dialog( "open" );
-		} );
+    // Add the callback
+    $('#exportorder').submit(function() {
+      $('#dialogcontents').dialog("close");
+      $('#postsubmitmessage').dialog("open");
+    });
 
     var completeOrder = new Object();
     completeOrder.issue = order.issue;
     completeOrder.line_items = order.lineItems;
     completeOrder.order_total = order.getTotal();
 
-    var encoded = $.toJSON( completeOrder );
+    var encoded = $.toJSON(completeOrder);
 
     // Memory fails me as to why I did it like this rather than an
     // asynchronous call
-		$('#encodeddata').val( Base64.encode( encoded ) );
-		$('#exportorder').submit();
-	} );
-	
-	$( 'table#complete_order_form').click( function( event ) {
-		if ( $(event.target).is( 'input.delete_from_order') ) {
-			event.stopPropagation();
-			var previewsId = /id_(.*)/.exec( event.target.id )[1];
-			order.deleteFromOrder( previewsId );
-			calculateTotals();
-		
+    $('#encodeddata').val(Base64.encode(encoded));
+    $('#exportorder').submit();
+  });
+
+  $('table#complete_order_form').click(function(event) {
+    if ($(event.target).is('input.delete_from_order')) {
+      event.stopPropagation();
+      var previewsId = /id_(.*)/.exec(event.target.id)[1];
+      order.deleteFromOrder(previewsId);
+      calculateTotals();
+
       /**
        * This is some funkery that swaps classes around so 
        * that items that have been deleted from the order
        * are no longer selected.
        */
-			var elem = $( "input:checkbox[value='previews_" + previewsId + "']" );
-			elem.removeClass( "deletefromorder" );
-			elem.addClass( "addtoorder" );
-			elem.attr( 'checked', false );
-			
-			$( event.target ).closest( 'tr' ).remove();
-		}
-	});
+      var elem = $("input:checkbox[value='previews_" + previewsId + "']");
+      elem.removeClass("deletefromorder");
+      elem.addClass("addtoorder");
+      elem.attr('checked', false);
+
+      $(event.target).closest('tr').remove();
+    }
+  });
 }
 
-$(document).ready( function() {
+$(document).ready(function() {
   csv2datatable();
 
-	$('table#datatable').click( function(event) {
-    var table = $("table#datatable").dataTable( {
+  $('table#datatable').click(function(event) {
+    var table = $("table#datatable").dataTable({
       "bRetrieve": true,
-    } );
+    });
 
-		if ( $(event.target).is( 'input.addtoorder' ) ) {
-			// We're on the case
-			event.stopPropagation();
+    if ($(event.target).is('input.addtoorder')) {
+      // We're on the case
+      event.stopPropagation();
 
-      var aData = table.fnGetData( /row(\d+)/.exec( event.target.id )[1] );
+      var aData = table.fnGetData(/row(\d+)/.exec(event.target.id)[1]);
 
-			// Add the row to the order. The id is "row(number)"
-      var price = parseFloat( /&pound;(.*)/.exec( aData[2] )[1] );
+      // Add the row to the order. The id is "row(number)"
+      var price = parseFloat(/&pound;(.*)/.exec(aData[2])[1]);
 
-      var lineItem = new LineItem( aData[0], aData[1], price, aData[4] );
-      order.addToOrder( lineItem );
+      var lineItem = new LineItem(aData[0], aData[1], price, aData[4]);
+      order.addToOrder(lineItem);
 
-			// Check the checkbox
-			var elem = $( "#" + event.target.id );
-			elem.checked = true;
-			elem.removeClass( "addtoorder" );
-			elem.addClass( "deletefromorder" );
-		}
-		else if ( $(event.target).is( 'input.deletefromorder' ) ) {
-			event.stopPropagation();
-      var aData = table.fnGetData( /row(\d+)/.exec( event.target.id )[1] );
-      order.deleteFromOrder( aData[0] );
+      // Check the checkbox
+      var elem = $("#" + event.target.id);
+      elem.checked = true;
+      elem.removeClass("addtoorder");
+      elem.addClass("deletefromorder");
+    }
+    else if ($(event.target).is('input.deletefromorder')) {
+      event.stopPropagation();
+      var aData = table.fnGetData(/row(\d+)/.exec(event.target.id)[1]);
+      order.deleteFromOrder(aData[0]);
 
-			// Change the class so that we can toggle orders.
-			var elem = $( "#" + event.target.id );
-			elem.removeClass( "deletefromorder" );
-			elem.addClass( "addtoorder" );
-			elem.checked = false;
-		}
+      // Change the class so that we can toggle orders.
+      var elem = $("#" + event.target.id);
+      elem.removeClass("deletefromorder");
+      elem.addClass("addtoorder");
+      elem.checked = false;
+    }
     calculateTotals();
-	});
+  });
 
-	$('#dialogcontents').dialog( {
-		"modal" : true,
-		"autoOpen" : false,
-		"minWidth": 800,
-    beforeClose:function(event, ui) {
-			// On close, update quantities and comments
-			$('tr.orderrow').each(
-				function( i, tr ) {
-          var previewsId = /row_(.*)/.exec( tr.id )[1];
-					var inputs = tr.getElementsByTagName( "input" );
-					var quantity = $( inputs[0] ).spinner( "value" );
-					order.setQuantity( previewsId, quantity );
-					order.setComment( previewsId,  $( inputs[1] ).val() );
-				}
-			)
-			calculateTotals();
-			return true;
-		},
-	});
-	
-	$('#postsubmitmessage').dialog( {
-		"modal" : true,
-		"autoOpen" : false,
-	});
+  $('#dialogcontents').dialog({
+    "modal": true,
+    "autoOpen": false,
+    "minWidth": 800,
+    beforeClose: function(event, ui) {
+      // On close, update quantities and comments
+      $('tr.orderrow').each(function(i, tr) {
+        var previewsId = /row_(.*)/.exec(tr.id)[1];
+        var inputs = tr.getElementsByTagName("input");
+        var quantity = $(inputs[0]).spinner("value");
+        order.setQuantity(previewsId, quantity);
+        order.setComment(previewsId, $(inputs[1]).val());
+      })
+      calculateTotals();
+      return true;
+    },
+  });
 
-	$('#showorder').click( function( event ) {
-		calculateOrder();
-	} );
+  $('#postsubmitmessage').dialog({
+    "modal": true,
+    "autoOpen": false,
+  });
 
-} );
+  $('#showorder').click(function(event) {
+    calculateOrder();
+  });
+
+});
--- js/customerorder.js
+++ js/customerorder.js
@@ -1,73 +1,72 @@
-function CustomerOrder( issue ) {
+function CustomerOrder(issue) {
   this.issue = issue;
   this.lineItems = [];
 }
 
 CustomerOrder.prototype = {
-  findByPreviewsID:function( previewsID ) {
+  findByPreviewsID: function(previewsID) {
     var ret = -1;
-    for ( var idx = 0; idx < this.lineItems.length; idx++ ) {
-      if ( this.lineItems[idx].previews == previewsID ) {
+    for (var idx = 0; idx < this.lineItems.length; idx++) {
+      if (this.lineItems[idx].previews == previewsID) {
         ret = idx;
         break;
       }
     }
     return ret;
   },
-  addToOrder:function( lineItem ) {
-    this.lineItems.push( lineItem );
-//--------------------------------------------------
-// 
-//     this.lineItems.sort( function( a, b ) ) {
-//       return a.previews.localeCompare( b.previews );
-//     },
-// 
-//-------------------------------------------------- 
+  addToOrder: function(lineItem) {
+    this.lineItems.push(lineItem);
+    //--------------------------------------------------
+    // 
+    //     this.lineItems.sort( function( a, b ) ) {
+    //       return a.previews.localeCompare( b.previews );
+    //     },
+    // 
+    //-------------------------------------------------- 
   },
-  deleteFromOrder:function( previewsId ) {
-    var idx = this.findByPreviewsID( previewsId );
+  deleteFromOrder: function(previewsId) {
+    var idx = this.findByPreviewsID(previewsId);
 
-    if ( idx > -1 ) {
-      this.lineItems.splice( idx, 1 );
+    if (idx > -1) {
+      this.lineItems.splice(idx, 1);
     }
   },
-  setQuantity:function( previewsId, quantity ) {
-    if ( quantity == 0 ) {
-      this.deleteFromOrder( previewsId );
-    }
-    else {
-      var idx = this.findByPreviewsID( previewsId );
-      if ( idx > -1 ) {
+  setQuantity: function(previewsId, quantity) {
+    if (quantity == 0) {
+      this.deleteFromOrder(previewsId);
+    } else {
+      var idx = this.findByPreviewsID(previewsId);
+      if (idx > -1) {
         this.lineItems[idx].quantity = quantity;
       }
     }
   },
-  setComment:function( previewsId, comment ) {
-      var idx = this.findByPreviewsID( previewsId );
-      if ( idx > -1 ) {
-        this.lineItems[idx].comment = comment;
-      }
+  setComment: function(previewsId, comment) {
+    var idx = this.findByPreviewsID(previewsId);
+    if (idx > -1) {
+      this.lineItems[idx].comment = comment;
+    }
   },
-  getTotal:function() {
+  getTotal: function() {
     var total = 0;
-    for ( var i in this.lineItems ) {
-      total += (this.lineItems[i].price * 100 ) * this.lineItems[i].quantity;
+    for (var i in this.lineItems) {
+      total += (this.lineItems[i].price * 100) * this.lineItems[i].quantity;
     }
     return total;
   },
-  getNumItems:function() {
+  getNumItems: function() {
     var total = 0;
-    for ( var i in this.lineItems ) {
+    for (var i in this.lineItems) {
       total += this.lineItems[i].quantity;
     }
     return total;
   },
-  getNumTitles:function() {
+  getNumTitles: function() {
     return this.lineItems.length;
   },
 }
 
-function LineItem( previewsID, title, price, publisher ) {
+function LineItem(previewsID, title, price, publisher) {
   this.previews = previewsID;
   this.title = title;
   this.price = price;
@@ -77,5 +76,5 @@ function LineItem( previewsID, title, price, publisher ) {
 }
 
 LineItem.prototype = {
-  constructor:LineItem,
+  constructor: LineItem,
 }
