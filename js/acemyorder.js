7a7a5434b7b1943498f017be625ee1a2741ae426
Ross Hendry <ross@rosshendry.com>
2014-01-09 23:33:11 +0000

    Again...

--- js/acemyorder.js
+++ js/acemyorder.js
@@ -95,7 +95,7 @@ function csv2datatable(sURL) {
         }
 
         // Publisher
-        row[4] = csvdata[i][6].toLowerCase();
+        row[4] = csvdata[i][7].toLowerCase();
 
         // What will eventually be the button
         row[5] = "";
